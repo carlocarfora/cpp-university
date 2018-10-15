5a177c9908b48c0b9d6f07a20be124f0c28d0f51
carloc <carlo@carlocarfora.co.uk>
2018-10-15 23:21:46 +0100

    updated homework

--- week04/homework/main.cpp
+++ week04/homework/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <cmath>
 
 int main() {
     std::cout << "Week 4 homework!\n\n" << std::endl;
@@ -34,31 +36,62 @@ int main() {
     }
 
     if (choice == 'B') {
-        std::cout << "Input a number, starting min and max is 0\n\n";
+        double sum = {};
+        double median = {};
+
+        std::cout << "Input a number sequence, enter a letter when done. \n\n";
 
         std::vector<double> sequence;
         double input = {};
-        double max = 0;
-        double min = 0;
 
         while (std::cin >> input) {
             sequence.push_back(input);
-
-            if (input < min) {
-                min = input;
-                std::cout << "New minimum is " << min << "\n";
-            } else if (input > max) {
-                max = input;
-                std::cout << "New maximum is " << max << "\n";
-            } else {
-                std::cout << "Number " << input << " is neither minimum or maximum\n";
-            }
         }
+
+        std::cout << "You entered as a sequence: ";
+        for (auto i : sequence) {
+            std::cout << i <<",";
+            sum += i;
+        }
+
+        // calculate mean
+        auto result = std::minmax_element(sequence.begin(), sequence.end());
+        auto mean = sum / sequence.size();
+
+        // calculate median
+        auto sorted = sequence;
+        std::sort(std::begin(sorted), std::end(sorted));
+
+        if (sequence.size() % 2 != 0) {
+            auto midpoint = sequence.size() / 2;
+            median = sorted[midpoint];
+        }
+        else {
+            auto midpoint = sequence.size() / 2;
+            auto ceil = sorted[midpoint - 1];
+            auto floor = sorted[midpoint];
+            median = (ceil + floor) / 2;
+        }
+
+        // calculate mode
+
+        // TO DO
+
+        // display results
+        std::cout << "\n\nThe minimum number is " << *result.first << "\n";
+        std::cout << "The maximum number is " << *result.second << "\n";
+        std::cout << "The mean is " << mean << "\n";
+        std::cout << "The median is " << median << "\n\n";
+
+
+        std::cout << "You entered as a sequence: ";
+        for (auto i : sorted) {
+            std::cout << i << ",";
+        }
+
+
     }
-//    for (auto i : sequence) {
-//
-//        std::cout << i;
-//    }
+
 
     return 0;
 }
